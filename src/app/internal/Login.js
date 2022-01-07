993f7e35d400967f71c79e6b486c81928d52aeda
Ryan Cocuzzo <rcocuzzo8@gmail.com>
2022-01-07 05:30:10 -0500

    ...

--- src/app/internal/Login.js
+++ src/app/internal/Login.js
@@ -1,176 +1,199 @@
-import React, {useState} from 'react';
-import { Form, DropdownButton, Button, Modal, ProgressBar, Row, Col, Dropdown } from 'react-bootstrap';
-import { URLs } from './Helper-Files/constants';
+import React, { useState } from "react";
+import { Button, Form, Modal, ProgressBar } from "react-bootstrap";
+import { URLs } from "./Helper-Files/constants";
+import { TEST_MODE } from "../../helpers/constants";
 
+// MARK - Helpers
+
+/**
+ * @param {String} email
+ * @returns whether the email is valid.
+ */
 function testEmail(email) {
-    return email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  return email.match(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
 }
 
+// MARK - Components
+
+/** Backend Notification */
 function Notification(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            {props.title}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            {props.description}
-          </p>
-        </Modal.Body>
-      </Modal>
-    );
-  }
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>{props.description}</p>
+      </Modal.Body>
+    </Modal>
+  );
+}
 
-async function wait(ms){
-    var start = new Date().getTime();
-    var end = start;
-    while(end < start + ms) {
-      end = new Date().getTime();
-   }
-   Promise.resolve('ok');
- }
-
-
-
+/**
+ * @param {*} props
+ * @returns Login component
+ */
 function Login(props) {
+  // Test variables
+  const TEST_OVERRIDE = false;
 
-    // Test variables
+  let EMAILVAL = process.env.EMAIL_CREDENTIAL;
+  let PASSVAL = process.env.PASS_CREDENTIAL;
 
-    const TEST_OVERRIDE = false;
-    const TEST = true;
+  const INIT_EM = TEST_MODE ? EMAILVAL : "";
+  const INIT_PS = TEST_MODE ? PASSVAL : "";
 
-    let EMAILVAL = 'rcocuzzo@u.rochester.edu';
-    let PASSVAL = 'Pablothepreacher71';
+  // State variables
 
-    const INIT_EM = TEST ? EMAILVAL : '';
-    const INIT_PS = TEST ? PASSVAL : '';
+  const [validated, setValidated] = useState(false);
+  const [showInvalidModal, setShowInvalidModal] = useState(false);
+  const [email, setEmail] = useState(INIT_EM);
+  const [password, setPassword] = useState(INIT_PS);
+  const [loginStatus, setLoginStatus] = useState(-1);
 
+  // Formally submit login info & begin handling responses
+  const SubmitLogin = async () => {
+    setLoginStatus(0);
 
-
-    const [validated, setValidated] = useState(false);
-    const [showInvalidModal, setShowInvalidModal] = useState(false);
-    const [email, setEmail] = useState(INIT_EM);
-    const [password, setPassword] = useState(INIT_PS);
-    const [loginStatus, setLoginStatus] = useState(-1);
-    const [session_id, setSessionId] = useState(null);
-    const [folderData, setFolderData] = useState(null);
-
-    // Formally submit login info & begin handling responses
-    const SubmitLogin = async () => {
-        setLoginStatus(0);
-        // Login & get data
-        const ws = new WebSocket('ws://' + URLs.NODE_LOGIN);
-        ws.onopen = function() {
-            console.log('WebSocket Client Connected : login');
-            ws.send(JSON.stringify({email: email, password: password}));
-        };
-        ws.onmessage = function(e) {
-          let got = JSON.parse(e.data);
-          if (Object.keys(got).length === 0) { //  bad data
-            setShowInvalidModal(true);
-            return;
-          }
-          // get info
-          let status = got.status;
-          let data =   got.data;
-          let session = data ? data.session : null;
-          console.log("Received: " + JSON.stringify(got, null, 2));
-          let directory = data ? data.dir : data;
-          // set progress
-          setLoginStatus(status);
-          setSessionId(session);
-          setFolderData(directory);
-          // Check for compeltion 
-          if (status === 100)
-            props.callback({session_id: session, data: directory});
-      };
-        return;
-    }
-    // handle / verify user info submission -> 
-    const handleSubmit = (event) => {
-      const form = event.currentTarget;
-      if (!TEST_OVERRIDE && ( form.checkValidity() === false || !email || !password || !testEmail(email) ) ) {
+    // Login & get data
+    const ws = new WebSocket("ws://" + URLs.NODE_LOGIN);
+    ws.onopen = function () {
+      console.log("WebSocket Client Connected : login");
+      // send data to web socket
+      ws.send(JSON.stringify({ email: email, password: password }));
+    };
+    ws.onmessage = function (e) {
+      // Parse web socket data
+      let got = JSON.parse(e.data);
+      if (Object.keys(got).length === 0) {
+        // Bad data. Show invalid.
         setShowInvalidModal(true);
-        event.preventDefault();
-        event.stopPropagation();
         return;
       }
-      
-      // form is valid
-      setValidated(true);
-      // submit login, move to next step.
-      SubmitLogin();
+      const { status, data } = got;
+      let session = data ? data.session : null;
+      let directory = data ? data.dir : data;
+      // set progress
+      setLoginStatus(status);
+      // Check for compeltion
+      if (status === 100)
+        props.callback({ session_id: session, data: directory });
     };
-
-    // labels for user's progress
-    const loginProgressLabel = () => {
-        let status = loginStatus;
-        if (status === 0) return 'Creating new HUDL Session';
-        if (status === 25) return 'Self-authenticating in HUDL';
-        if (status === 50) return 'Finding your folders.';
-        if (status === 75) return 'Parsing Folders.';
-        else return 'Done.';
+    return;
+  };
+  // handle / verify user info submission ->
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    // Verify the form is invalid and that the form should not be overriden.
+    if (
+      !TEST_OVERRIDE &&
+      (form.checkValidity() === false ||
+        !email ||
+        !password ||
+        !testEmail(email))
+    ) {
+      setShowInvalidModal(true);
+      event.preventDefault();
+      event.stopPropagation();
+      return;
     }
 
-    let LoginProgressBar = (
-        <div class='logging_in'>
-            <h2>Logging in.</h2>
-            <ProgressBar now={loginStatus} />
-            <p>{loginProgressLabel()}</p>
-        </div>
-    );
+    // form is valid
+    setValidated(true);
+    // submit login, move to next step.
+    SubmitLogin();
+  };
 
-    
+  /**
+   * @returns {String} labels for user's progress
+   */
+  const loginProgressLabel = () => {
+    let status = loginStatus;
+    if (status === 0) return "Creating new HUDL Session";
+    if (status === 25) return "Self-authenticating in HUDL";
+    if (status === 50) return "Finding your folders.";
+    if (status === 75) return "Parsing Folders.";
+    else return "Done.";
+  };
 
-    const invalidModal = (
-        <Notification
-            title='Invalid credentials'
-            description='Please retry login with correct credentials'
-            show={showInvalidModal}
-            onHide={() => setShowInvalidModal(false)}
+  // Login progress bar
+  const LoginProgressBar = (
+    <div class="logging_in">
+      <h2>Logging in.</h2>
+      <ProgressBar now={loginStatus} />
+      <p>{loginProgressLabel()}</p>
+    </div>
+  );
+
+  // Invalid Credentials modal
+  const invalidModal = (
+    <Notification
+      title="Invalid credentials"
+      description="Please retry login with correct credentials"
+      show={showInvalidModal}
+      onHide={() => setShowInvalidModal(false)}
+    />
+  );
+
+  /**
+   * Propagate email change.
+   * @param {*} e the event
+   */
+  const emailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  /**
+   * Propagate password change.
+   * @param {*} e the event
+   */
+  const passChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  // Login form
+  const LoginForm = (
+    <Form noValidate validated={validated}>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          onChange={emailChange}
+          value={TEST_MODE ? EMAILVAL : ""}
+          type="email"
+          placeholder="Enter email"
         />
-    );
-  
-    const emailChange = (e) => {
-        setEmail(e.target.value);
-    }
-    const passChange = (e) => {
-        setPassword(e.target.value);
-    }
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-    const LoginForm = (
-        <Form noValidate validated={validated}>
-        <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control onChange={emailChange} value={TEST ? EMAILVAL : ''} type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-            </Form.Text>
-        </Form.Group>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          onChange={passChange}
+          value={TEST_MODE ? PASSVAL : ""}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Group>
+      <Button onClick={handleSubmit}>Login</Button>
+      {invalidModal}
+    </Form>
+  );
 
-        <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control onChange={passChange}value={TEST ? PASSVAL : ''} type="password" placeholder="Password" />
-        </Form.Group>
-        <Button onClick={handleSubmit}>Login</Button>
-        {invalidModal}
-      </Form>
-    );
-
-
-    if (loginStatus === 100) return null;
-    return (
-        <div>
-            {loginStatus >= 0 ? LoginProgressBar : LoginForm}
-        </div>
-    );
-  }
+  // If the user is logged in, display nothing.
+  if (loginStatus === 100) return null;
+  // Otherwise, display the form or the current loading progress.
+  return <div>{loginStatus >= 0 ? LoginProgressBar : LoginForm}</div>;
+}
 
 export default Login;
--- src/app/internal/Navigation.js
+++ src/app/internal/Navigation.js
@@ -1,133 +1,185 @@
-import React, {useState} from 'react';
-import { Accordion, Card, Button } from 'react-bootstrap';
-import './nav.css'
-import {useAccordionToggle} from 'react-bootstrap/AccordionToggle';
-import Login from '../../Login';
-import {folder_data} from '../../test/testdata';
-import SelectFolder from './SelectFolder';
-import SelectFilm from './SelectFilm';
-import ViewData from './ViewData';
-import Predictions from './Predictions';
-import SelectTrainingData from "./SelectTrainingData";
+import React, { useState } from "react";
+import { Accordion, Card } from "react-bootstrap";
+import { useAccordionToggle } from "react-bootstrap/AccordionToggle";
+import Login from "../../Login";
+import "./nav.css";
+import Predictions from "./Predictions";
 import SelectTestData from "./SelectTestData";
+import SelectTrainingData from "./SelectTrainingData";
+import ViewData from "./ViewData";
 
+/* MARK - Components */
 
-/* CUSTOM TOGGLING OPTION */
+/**
+ * A "done"-togglable component
+ * @param {*} param0 Children components
+ * @returns the toggle component
+ */
 function CustomToggle({ children, eventKey, done }) {
-    const decoratedOnClick = useAccordionToggle(eventKey, () =>
-      console.log('totally custom!'),
-    );
+  const decoratedOnClick = useAccordionToggle(
+    eventKey,
+    () => {} // do nothing
+  );
 
-    let done_icon = (
-        <svg class="icon icon-check-circle"><use xlinkHref="#icon-check-circle">
+  let done_icon = (
+    <svg class="icon icon-check-circle">
+      <use xlinkHref="#icon-check-circle">
         <symbol id="icon-check-circle" viewBox="0 -8 24 38">
-        <path d="M20.062 11.469c0-0.266-0.094-0.531-0.281-0.719l-1.422-1.406c-0.187-0.187-0.438-0.297-0.703-0.297s-0.516 0.109-0.703 0.297l-6.375 6.359-3.531-3.531c-0.187-0.187-0.438-0.297-0.703-0.297s-0.516 0.109-0.703 0.297l-1.422 1.406c-0.187 0.187-0.281 0.453-0.281 0.719s0.094 0.516 0.281 0.703l5.656 5.656c0.187 0.187 0.453 0.297 0.703 0.297 0.266 0 0.531-0.109 0.719-0.297l8.484-8.484c0.187-0.187 0.281-0.438 0.281-0.703zM24 14c0 6.625-5.375 12-12 12s-12-5.375-12-12 5.375-12 12-12 12 5.375 12 12z"></path>
+          <path d="M20.062 11.469c0-0.266-0.094-0.531-0.281-0.719l-1.422-1.406c-0.187-0.187-0.438-0.297-0.703-0.297s-0.516 0.109-0.703 0.297l-6.375 6.359-3.531-3.531c-0.187-0.187-0.438-0.297-0.703-0.297s-0.516 0.109-0.703 0.297l-1.422 1.406c-0.187 0.187-0.281 0.453-0.281 0.719s0.094 0.516 0.281 0.703l5.656 5.656c0.187 0.187 0.453 0.297 0.703 0.297 0.266 0 0.531-0.109 0.719-0.297l8.484-8.484c0.187-0.187 0.281-0.438 0.281-0.703zM24 14c0 6.625-5.375 12-12 12s-12-5.375-12-12 5.375-12 12-12 12 5.375 12 12z"></path>
         </symbol>
-        </use></svg>
-    );
-  
-    return (
-        <div class='processnavitem' onClick={done ? () => {} : decoratedOnClick}>
-        <h2>{children}</h2>
-        {done ? done_icon : null}
+      </use>
+    </svg>
+  );
+
+  return (
+    <div class="processnavitem" onClick={done ? () => {} : decoratedOnClick}>
+      <h2>{children}</h2>
+      {done ? done_icon : null}
     </div>
-    );
-  }
+  );
+}
 
-
-
-  /* NAVIGATION */
-
+/**
+ * @returns the Navigation component
+ */
 function Navigation() {
+  // Setup state variables
+  const [step, setStep] = useState(1);
+  const [allFolders, setAllFolders] = useState(null);
+  const [session_id, setSessionId] = useState(null);
+  const [nn_model, set_NN_Modal] = useState(null);
+  const [dataDomain, set_DataDomain] = useState(null);
+  const [output_indices, set_OutputIndices] = useState(null);
+  const [clean_headers, set_CleanHeaders] = useState(null);
+  const [clean_data_indices, set_CleanDataIndices] = useState(null);
+  const [trainingData, set_TrainingData] = useState(null);
+  const [str_trainingData, set_Str_TrainingData] = useState(null);
+  const [testData, set_TestData] = useState(null);
+  const [str_testData, set_Str_TestData] = useState(null);
+  const [training_arrays, set_TrainingArrays] = useState(null);
 
-    const [step, setStep] = useState(1);
-    const [allFolders, setAllFolders] = useState(null);
-    const [session_id, setSessionId] = useState(null);
-    // const [videos, setVideos] = useState(null);
-    const [filmData_withHeaders, set_FilmData_withHeaders] = useState(null);
-    const [nn_model, set_NN_Modal] = useState(null);
-    const [dataDomain, set_DataDomain] = useState(null); 
-    const [output_indices, set_OutputIndices] = useState(null); 
-    const [clean_headers, set_CleanHeaders] = useState(null);
-    const [clean_data_indices, set_CleanDataIndices] = useState(null);
-    const [trainingData, set_TrainingData] = useState(null);
-    const [str_trainingData, set_Str_TrainingData] = useState(null);
-    const [testData, set_TestData] = useState(null);
-    const [str_testData, set_Str_TestData] = useState(null);
-    const [training_arrays, set_TrainingArrays] = useState(null);
-    
-    var logged_in = (result) => {
-        setSessionId(result.session_id);
-        setAllFolders(result.data);
-        setStep(step+1);
-    }
-    // var selected_folder = (result) => {
-    //     setVideos(result);
-    //     setStep(step+1);
-    // }
-    var selected_film = (result) => {
-        set_FilmData_withHeaders(result);
-        setStep(step+1);
-    }
-    var viewed_data_and_generated_nn_model = (model, dataDomain, output_indices) => {
-        set_NN_Modal(model);
-        set_DataDomain(dataDomain);
-        console.log('Setting output indices to: ' + output_indices)
-        set_OutputIndices(output_indices);
-        setStep(step+1);
-    }
+  /**
+   * Callback for when a user logged in.
+   * Sets up session info and folder info.
+   * @param {Object} result the session result
+   */
+  const logged_in = (result) => {
+    setSessionId(result.session_id);
+    setAllFolders(result.data);
+    setStep(step + 1);
+  };
 
-    var selected_training_data = (result) => {
-        set_CleanHeaders(result.headers);
-        set_TrainingData(result.data);
-        set_Str_TrainingData(result.unparsedData);
-        set_CleanDataIndices(result.clean_indices);
-        set_TrainingArrays(result.training_arrays);
-        setStep(step+1);
-    }
+  /**
+   * Callback when a user has generated the Tf models.
+   * @param {Tf.model} model Tf model
+   * @param {*} dataDomain the data domain
+   * @param {*} output_indices the indices of the data's output columns
+   */
+  const viewed_data_and_generated_nn_model = (
+    model,
+    dataDomain,
+    output_indices
+  ) => {
+    set_NN_Modal(model);
+    set_DataDomain(dataDomain);
+    console.log("Setting output indices to: " + output_indices);
+    set_OutputIndices(output_indices);
+    setStep(step + 1);
+  };
 
-    var selected_test_data = (result) => {
-        set_TestData(result.data);
-        set_Str_TestData(result.unparsedData);
-        setStep(step+1);
-    } 
+  /**
+   * Callback when the training data has been selected.
+   * @param {Object} result the polished data result
+   */
+  const selected_training_data = (result) => {
+    set_CleanHeaders(result.headers);
+    set_TrainingData(result.data);
+    set_Str_TrainingData(result.unparsedData);
+    set_CleanDataIndices(result.clean_indices);
+    set_TrainingArrays(result.training_arrays);
+    setStep(step + 1);
+  };
 
-    const AccordionCard = (threshold, title, body) => {
-        return (
-            <Card>
-                <Card.Header>
-                    <CustomToggle eventKey="0" done={step > threshold}>{title}</CustomToggle>
-                </Card.Header>
-                {step == threshold ? 
-                <Accordion.Collapse eventKey="0">
-                    <Card.Body>
-                        
-                        <div style={{display: 'block'}}>
-                            {body}
-                        </div>
-                    </Card.Body>
-                </Accordion.Collapse>
-                : null }
-            </Card>
-        );
-    }
+  /**
+   * Callback when the user has selected the test data.
+   * @param {Object} result the polished data result
+   */
+  const selected_test_data = (result) => {
+    set_TestData(result.data);
+    set_Str_TestData(result.unparsedData);
+    setStep(step + 1);
+  };
 
+  /* Accordion Card / Subview for the different views being presented */
+  const AccordionCard = (threshold, title, body) => {
     return (
-        <div class='navmenu'>
-            <Accordion defaultActiveKey="0">
-            {AccordionCard(1,'HUDL Login', <Login callback={logged_in} />)}
-            {AccordionCard(2,'Select Training Data', <SelectTrainingData session_id={session_id} data={allFolders} callback={selected_training_data} />)}
-            {AccordionCard(3,'Select Test Data', <SelectTestData session_id={session_id} data={allFolders} training_arrays={training_arrays} str_trainingdata={str_trainingData} used_indices={clean_data_indices}  callback={selected_test_data} />)}
-            {/* {AccordionCard(2,'Select Film Folder', <SelectFolder session_id={session_id} data={allFolders} callback={selected_folder} />)} */}
-            {/* {AccordionCard(3,'Select Film', <SelectFilm session_id={session_id} videos={videos} callback={selected_film} />)} */}
-            {AccordionCard(4,'View Data', <ViewData training={trainingData} test={testData} str_training_data={str_trainingData} headers={clean_headers} str_test_data={str_testData} callback={viewed_data_and_generated_nn_model} />)}
-            {AccordionCard(5,'Get Predictions', <Predictions model={nn_model} dataDomain={dataDomain} headers={clean_headers} outputs={output_indices} />)}
-            </Accordion>
-        </div>
-        
+      <Card>
+        <Card.Header>
+          <CustomToggle eventKey="0" done={step > threshold}>
+            {title}
+          </CustomToggle>
+        </Card.Header>
+        {step == threshold ? (
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <div style={{ display: "block" }}>{body}</div>
+            </Card.Body>
+          </Accordion.Collapse>
+        ) : null}
+      </Card>
     );
+  };
 
-
+  return (
+    <div class="navmenu">
+      <Accordion defaultActiveKey="0">
+        {AccordionCard(1, "HUDL Login", <Login callback={logged_in} />)}
+        {AccordionCard(
+          2,
+          "Select Training Data",
+          <SelectTrainingData
+            session_id={session_id}
+            data={allFolders}
+            callback={selected_training_data}
+          />
+        )}
+        {AccordionCard(
+          3,
+          "Select Test Data",
+          <SelectTestData
+            session_id={session_id}
+            data={allFolders}
+            training_arrays={training_arrays}
+            str_trainingdata={str_trainingData}
+            used_indices={clean_data_indices}
+            callback={selected_test_data}
+          />
+        )}
+        {AccordionCard(
+          4,
+          "View Data",
+          <ViewData
+            training={trainingData}
+            test={testData}
+            str_training_data={str_trainingData}
+            headers={clean_headers}
+            str_test_data={str_testData}
+            callback={viewed_data_and_generated_nn_model}
+          />
+        )}
+        {AccordionCard(
+          5,
+          "Get Predictions",
+          <Predictions
+            model={nn_model}
+            dataDomain={dataDomain}
+            headers={clean_headers}
+            outputs={output_indices}
+          />
+        )}
+      </Accordion>
+    </div>
+  );
 }
 
 export default Navigation;
